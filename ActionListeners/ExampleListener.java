917e351104f2c82d7082c5f8d233d5a0cef9ca45
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2016-12-22 13:52:47 -0500

    changed the example listener

--- ActionListeners/ExampleListener.java
+++ ActionListeners/ExampleListener.java
@@ -13,8 +13,23 @@ public class ExampleListener implements ActionListener
 	public void actionPerformed(ActionEvent e)
 	{
 		try
-		{
-			d.getDCname();
+		{	
+			/*d.setDCName();
+			d.setDCStudent_ID();
+			d.setDCA1();
+			d.setDCA2();
+			d.setDCA3();
+			d.setDCA4();
+			d.setDCAverage();*/
+
+			/*d.getDCName();
+			d.getDCStudent_ID();
+			d.getDCA1();
+			d.getDCA2();
+			d.getDCA3();
+			d.getDCA4();
+			d.getDCAverage();*/
+			
 			d.appendToStatusArea("button pressed.");
 		}
 		catch(Exception tr)
